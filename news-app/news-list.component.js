2586f110731b15e18dffd212201792852a56f3e5
unknown <v.pikulska@VPIKULSKA.levi9.com>
2016-11-21 15:46:03 +0100

    Level 2 Add refresh button on top of the news list.

--- news-app/news-list.component.js
+++ news-app/news-list.component.js
@@ -9,14 +9,17 @@
         $scope.error ="Sorry, we couldn't find news for you. Please try again later.";
         $log.info = error;
      };
-        var getNewsList = function(response){
+         var getNewsList = function(response){
             
             $scope.response = response.data;
             
         };
-        $http.get("http://content.guardianapis.com/search?api-key=test/")
-                    .then(getNewsList, onError);
-                       
+        $scope.getNews = function () {
+            $http.get("http://content.guardianapis.com/search?api-key=test")
+                 .then(getNewsList, onError);
+             };  
+        $http.get("http://content.guardianapis.com/search?api-key=test")
+                 .then(getNewsList, onError);        
                    
     };
    
