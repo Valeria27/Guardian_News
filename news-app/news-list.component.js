5904f0d566df2d2cc66a6bd8cc49ef384ca37327
unknown <v.pikulska@VPIKULSKA.levi9.com>
2016-11-21 14:46:29 +0100

    Level 1 Create a web app that shows a list of 10 last news from The Guardian API.

---
+++ news-app/news-list.component.js
@@ -0,0 +1,25 @@
+(function() {
+     
+     var app = angular.module("newsApp")
+
+     
+     var fetchNewsController = function($scope, $http) {
+
+        var onError = function(error){
+        $scope.error ="Sorry, we couldn't find news for you. Please try again later.";
+        $log.info = error;
+     };
+        var getNewsList = function(response){
+            
+            $scope.response = response.data;
+            
+        };
+        $http.get("http://content.guardianapis.com/search?api-key=test/")
+                    .then(getNewsList, onError);
+                       
+                   
+    };
+   
+
+app.controller ("fetchNewsController",fetchNewsController)
+} ());
