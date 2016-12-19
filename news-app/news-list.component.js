3d592757700615481e16eacf314430e06a5a3b3b
unknown <v.pikulska@VPIKULSKA.levi9.com>
2016-12-19 12:31:56 +0100

    Level 3 - updates.

--- news-app/news-list.component.js
+++ news-app/news-list.component.js
@@ -4,15 +4,17 @@
     function NewsListController(newsService) {
     	var model = this;
 
+        model.currentPage = 1;
+        model.pageFromInput = model.currentPage;
+
     	var onRequestError = function(error){
         	model.error = 'Sorry, we couldn\'t find news for you. Please try again later.';
     	};
 
-
-    	model.getNewsList= function(){
-    		model.error = '';
+    	model.getNewsList = function(){   //Used for getting list of news for current page only (Default value current page =1 )
+            model.error = '';
        		newsService
-       			.getNewsList()
+       			.getNewsList(model.currentPage)
        			.then(function(newsList){
 	            	model.newsList = newsList;
 	        	}, onRequestError);
@@ -20,8 +22,20 @@
 
     	model.getNewsList();
 
-        model.onSelectNewsItem = function(newsItem) {
+        model.getNewsPages = function(){  //Used for getting value of Total pages (http call)
+            model.error = '';
+            newsService
+                .getNewsPages()
+                .then(function(totalPages){
+                    model.totalPages = totalPages;
+                }, onRequestError);
+        };
+
+        model.getNewsPages();
+
+        model.onSelectNewsItem = function(newsItem) { //Used for getting news details (text of an article)
         	if (!newsItem.newsItemDetails){
+                model.error = '';
             	newsService
             		.getNewsItemDetails(newsItem)
             		.then(function(newsItemDetails){
@@ -29,6 +43,26 @@
 	        		}, onRequestError);
 	        };
         };
+
+        model.onPageFromInputChange = function(){
+            if (model.pageFromInput){
+                model.currentPage = model.pageFromInput;
+                model.getNewsList();
+            }
+        };
+
+        model.goToPreviousPage = function (){
+            model.currentPage = model.currentPage - 1;
+            model.pageFromInput = model.currentPage;
+            model.getNewsList()
+        };
+        model.goToNextPage = function (){
+            model.currentPage = model.currentPage + 1;
+            model.pageFromInput = model.currentPage;
+            model.getNewsList()
+        };
+
+
     }
 
     var module = angular.module('newsApp');
--- news-app/news-service.js
+++ news-app/news-service.js
@@ -2,23 +2,31 @@
     'use strict';
 
     function NewsService($http) {
-        var getNewsList = function () {
-            return  $http.get('http://content.guardianapis.com/search?api-key=test')
+        var getNewsList = function (currentPage) { //Used for getting list of news for current page only (Default value current page =1 )
+            return  $http.get('http://content.guardianapis.com/search?page=' + currentPage + '&api-key=test')
                     .then(function(response){
                         return response.data.response.results;
                     });
         };
 
-        var getNewsItemDetails = function(newsItem){
+        var getNewsItemDetails = function(newsItem){ //Used for getting news details (text of an article)
             return  $http.get(newsItem.apiUrl + '?show-blocks=body&api-key=test')
                     .then(function(response) {
                         return response.data.response.content.blocks.body[0].bodyTextSummary;
                     });
         };
 
+        var getNewsPages = function () {    //Used for getting value of Total pages
+            return  $http.get('http://content.guardianapis.com/search?api-key=test')
+                    .then(function(response){
+                        return response.data.response.pages;
+                    });
+        };
+
         return {
             getNewsList: getNewsList,
             getNewsItemDetails: getNewsItemDetails,
+            getNewsPages: getNewsPages
         };
     }
 
